68a4321e788c837307ded84db0ac58ce0adf3757
Jerson Satoru Uyekita <jersonsatoru@yahoo.com.br>
2023-07-29 12:00:54 -0300

    feat: add keycloak login

---
+++ README.md
@@ -0,0 +1,17 @@
+### Pre-requisites
+
+- Docker
+- docker-compose
+
+### Running project locally
+
+```code
+    docker-compose up
+```
+
+### To execute the flow:
+
+- you'll need to access __localhost:8080/admin__
+- create a new realm
+- create a user to the realm
+- create a new client
--- authorization-flow/src/main.ts
+++ authorization-flow/src/main.ts
@@ -1,11 +1,37 @@
 import express, { Request, Response } from 'express';
+import envVar from 'env-var';
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (request: Request, response: Response) => {
+interface Config {
+    keycloakAuthURL: string
+    keycloakClientID: string
+    appPort: number
+}
+
+const configs: Config = {
+    keycloakAuthURL: envVar.get('KEYCLOAK_AUTH_URL').required().asString(),
+    keycloakClientID: envVar.get('KEYCLOAK_CLIENT_ID').required().asString(),
+    appPort: envVar.get('APP_PORT').required().asPortNumber(),
+}
+
+app.get('/login', async (request: Request, response: Response) => {
+    const loginParams = new URLSearchParams({
+        client_id: configs.keycloakClientID,
+        redirect_uri: 'http://app:3001/callback',
+        response_type: 'code',
+        scope: 'openid'
+    });
+
+    response.redirect(`${configs.keycloakAuthURL}?${loginParams.toString()}`)
+})
+
+app.get('/', async (request: Request, response: Response) => {
     response.json({
         message: 'Hello World',
     });
 });
 
-app.listen(3000);
+app.listen(configs.appPort);
